ff6e0bc59b88cde9d3265c8d8bb02e8ad7aa8c8e
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-10-23 13:01:37 +0500

    updated

--- dashboard/src/sections/Main.jsx
+++ dashboard/src/sections/Main.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FiTwitter } from "react-icons/fi";
+import client from "../assets/images/client.jpg";
+import client1 from "../assets/images/client1.jpg";
+import client2 from "../assets/images/client2.jpg";
+import client3 from "../assets/images/client3.jpg";
+import client4 from "../assets/images/client4.jpg";
 
 const Main = () => {
   return (
     <section>
-      <Header/>
+      <Header />
     </section>
-  )
-}
+  );
+};
 
-export default Main
-
+export default Main;
