639e73d2b49506fd1d73eabf6150266f1bd91e0d
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-10-23 12:24:22 +0500

    start Overview section

--- dashboard/src/components/Header.jsx
+++ dashboard/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import {IoSearch} from 'react-icons/io5'
+import {IoIosArrowDown} from 'react-icons/io'
+import client from '../assets/images/client1.jpg'
 
 const Header = () => {
   return (
+    <section className='w-full bg-slate-100 lg:h-20 h-fit flex lg:flex-col justify-between items-center p-4 rounded-xl lg:gap-2 gap-4'>
     <div>
-      <h1>header</h1>
+      <h1 className='text-2xl font-semibold'>Overview</h1>
     </div>
+
+    </section>
   )
 }
 
--- dashboard/src/sections/Main.jsx
+++ dashboard/src/sections/Main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Main = () => {
   return (
-    <div>
-      
-      
-    </div>
+    <section>
+      <Header/>
+    </section>
   )
 }
 
